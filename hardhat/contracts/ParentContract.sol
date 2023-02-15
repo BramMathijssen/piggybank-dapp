566f5d1a14a1193f388b0b6a3d7f3275b16c8fd8
Bram <Bram.mathijssen@gmail.com>
2023-02-15 22:13:25 +0100

    added parent contract as a factory contract

--- hardhat/contracts/ParentContract.sol
+++ hardhat/contracts/ParentContract.sol
@@ -2,7 +2,25 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./TokenCreator.sol";
+import "hardhat/console.sol";
 
-abstract contract ParentContract {
+// Parent Contract is deployed only once to the goerli network 
+contract ParentContract {
+
+    TokenCreator public tokenCreator;
+    address [] public createdTokenAddresses;
+
+    // mapping of parent addresses to the coins they minted on this contract
+    mapping(address => address[]) parentToTokensMapping;
+
+    // what do we need on contract deployment?
     constructor() {}
+
+    function createNewToken(uint256 supply, string memory contractName, string memory contractSymbol) public{
+        address createdToken = address(new TokenCreator(supply, contractName, contractSymbol));
+
+        createdTokenAddresses.push(createdToken);
+        parentToTokensMapping[msg.sender].push(createdToken);
+    }
 }
--- hardhat/contracts/TokenCreator.sol
+++ hardhat/contracts/TokenCreator.sol
@@ -2,8 +2,11 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./ParentContract.sol";
 
 contract TokenCreator is ERC20 {
+
+    ParentContract public parentContract;
 
     constructor(uint256 initialSupply, string memory tokenNameInput, string memory symbolInput) ERC20(tokenNameInput, symbolInput) {
         _mint(msg.sender, initialSupply);
