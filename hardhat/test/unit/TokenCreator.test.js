a9f20e09d2fb5a783346119a7063f7c3fcc573a2
Bram <Bram.mathijssen@gmail.com>
2023-02-21 19:35:17 +0100

    added test for tokencreator contract

--- hardhat/test/unit/TokenCreator.test.js
+++ hardhat/test/unit/TokenCreator.test.js
@@ -3,16 +3,18 @@ const { network, deployments, ethers, getNamedAccounts } = require("hardhat")
 
 describe("Token Creator", function () {
     let deployer
+    const SUPPLY = "10000"
 
     beforeEach(async () => {
         deployer = (await getNamedAccounts()).deployer
         console.log(deployer)
         tokenCreatorFactory = await ethers.getContractFactory("TokenCreator")
-        tokenCreator = await tokenCreatorFactory.deploy(10000, "Brenk Token", "BRE")
+        tokenCreator = await tokenCreatorFactory.deploy(SUPPLY, "Brenk Token", "BRE")
     })
-    describe("constructor", async function () {
-        it("sets the aggregator address correctly", async function () {
-            console.log(`yeeeboiii`)
+    describe("Contract deployment", async function () {
+        it("sends the correct amount of tokens to the deployer", async function () {
+            const deployerBalance = (await tokenCreator.balanceOf(deployer)).toString()
+            expect(deployerBalance).to.equal(SUPPLY)
         })
     })
 })
