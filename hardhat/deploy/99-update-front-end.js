d20e788f8c28f9c7682448ef75eef217e70d2770
Bram <Bram.mathijssen@gmail.com>
2023-02-26 11:52:50 +0100

    added deploy script for parentContract

--- hardhat/deploy/99-update-front-end.js
+++ hardhat/deploy/99-update-front-end.js
@@ -2,39 +2,35 @@ require("dotenv").config()
 const fs = require("fs")
 const { ethers } = require("hardhat")
 
-//const frontEndContractsFile = "../frontend/constants/networkMapping.json"
+const frontEndContractsFile = "../frontend/src/constants/contractAddresses.json"
 const frontEndAbiLocation = "../frontend/src/constants/"
 
 module.exports = async () => {
     if (process.env.UPDATE_FRONT_END) {
         console.log("Writing to front end...")
-        // await updateContractAddresses()
-        // await updateAbi()
+        await updateContractAddresses()
+        await updateAbi()
         console.log("Front end written!")
     }
 }
 
 async function updateAbi() {
-    const tokenCreator = await ethers.getContract("TokenCreator")
+    const tokenCreator = await ethers.getContract("ParentContract")
     console.log(tokenCreator)
-    // fs.writeFileSync(
-    //     `${frontEndAbiLocation}TokenCreator.json`,
-    //     tokenCreator.interface.format(ethers.utils.FormatTypes.json)
-    // )
+    fs.writeFileSync(`${frontEndAbiLocation}parentContract.abi.json`, tokenCreator.interface.format(ethers.utils.FormatTypes.json))
 }
 
-// async function updateContractAddresses() {
-//     const chainId = network.config.chainId.toString()
-//     const nftMarketplace = await ethers.getContract("NftMarketplace")
-//     const contractAddresses = JSON.parse(fs.readFileSync(frontEndContractsFile, "utf8"))
-//     if (chainId in contractAddresses) {
-//         if (!contractAddresses[chainId]["NftMarketplace"].includes(nftMarketplace.address)) {
-//             contractAddresses[chainId]["NftMarketplace"].push(nftMarketplace.address)
-//         }
-//     } else {
-//         contractAddresses[chainId] = { NftMarketplace: [nftMarketplace.address] }
-//     }
-//     fs.writeFileSync(frontEndContractsFile, JSON.stringify(contractAddresses))
-//     fs.writeFileSync(frontEndContractsFile2, JSON.stringify(contractAddresses))
-// }
+async function updateContractAddresses() {
+    const chainId = network.config.chainId.toString()
+    const parentContract = await ethers.getContract("ParentContract")
+    const contractAddresses = JSON.parse(fs.readFileSync(frontEndContractsFile, "utf8"))
+    if (chainId in contractAddresses) {
+        if (!contractAddresses[chainId]["ParentContract"].includes(parentContract.address)) {
+            contractAddresses[chainId]["ParentContract"].push(parentContract.address)
+        }
+    } else {
+        contractAddresses[chainId] = { ParentContract: [parentContract.address] }
+    }
+    fs.writeFileSync(frontEndContractsFile, JSON.stringify(contractAddresses))
+}
 module.exports.tags = ["all", "frontend"]
