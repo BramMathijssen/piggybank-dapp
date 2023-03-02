57c7b88e27a09c9c320aaf2562ef1ec334612e96
Bram <Bram.mathijssen@gmail.com>
2023-03-02 18:02:25 +0100

    refactored child homepanel to single useEffect

--- frontend/src/components/ChildPage/Home/HomePanel.js
+++ frontend/src/components/ChildPage/Home/HomePanel.js
@@ -2,84 +2,53 @@ import { ethers } from "ethers";
 import React, { useContext, useEffect, useState } from "react";
 import EthersContext from "../../../context/ethers-context";
 import Timer from "../CountdownTimer";
-
 import styles from "./HomePanel.module.scss";
 
 const HomePanel = () => {
-    const [parentAddress, setParentAddress] = useState();
-    const [myClaim, setMyClaim] = useState();
-    const [currentTime, setCurrentTime] = useState();
-    const [nextClaimMoment, setNextClaimMoment] = useState();
-    const [timeLeft, setTimeLeft] = useState();
-    const ethersCtx = useContext(EthersContext);
+  const [parentAddress, setParentAddress] = useState();
+  const [myClaim, setMyClaim] = useState();
+  const [timeLeft, setTimeLeft] = useState();
+  const ethersCtx = useContext(EthersContext);
 
-    useEffect(() => {
-        const getMyParent = async () => {
-            const tx = await ethersCtx?.contract.childToParentMapping(ethersCtx.userAddress);
+  useEffect(() => {
+    const getMyParentAndClaim = async () => {
+      if (!ethersCtx.contract) return;
 
-            setParentAddress(tx);
-            console.log(tx);
-        };
-        ethersCtx.contract && getMyParent();
-    }, [ethersCtx]);
+      const parentTx = await ethersCtx.contract.childToParentMapping(ethersCtx.userAddress);
+      setParentAddress(parentTx);
 
-    useEffect(() => {
-        const getMyClaim = async () => {
-            const tx = await ethersCtx?.contract.parentToChildMappingNested(parentAddress, ethersCtx?.userAddress);
-            console.log(tx);
-            setMyClaim(tx);
-            const { nextClaimPeriod } = tx;
-            setNextClaimMoment(nextClaimPeriod.toNumber());
-            console.log(nextClaimPeriod.toString());
-        };
-        ethersCtx.contract && getMyClaim();
-    }, [ethersCtx, parentAddress]);
+      const claimTx = await ethersCtx.contract.parentToChildMappingNested(parentTx, ethersCtx?.userAddress);
+      setMyClaim(claimTx);
 
-    useEffect(() => {
-        const getCurrentTime = async () => {
-            const tx = await ethersCtx?.contract.getCurrentTime();
-            console.log(tx.toNumber());
-            setCurrentTime(tx.toString());
-        };
-        ethersCtx.contract && getCurrentTime();
-    }, [ethersCtx, parentAddress]);
+      const currentTime = await ethersCtx.contract.getCurrentTime();
+      const leftTime = claimTx.nextClaimPeriod.toNumber() - currentTime.toNumber();
+      setTimeLeft(leftTime);
+    };
+    getMyParentAndClaim();
+  }, [ethersCtx]);
 
-    useEffect(() => {
-        const leftTime = nextClaimMoment - currentTime;
-
-        console.log(`------time left--------`);
-        console.log(` ❓ Current time is: ${currentTime}`);
-        console.log(`✅ NextClaim Moment: ${nextClaimMoment} - Current Time: ${currentTime} = ${timeLeft}`);
-        console.log(timeLeft);
-
-        setTimeLeft(leftTime);
-    }, [currentTime, nextClaimMoment, timeLeft]);
-
-    console.log(parentAddress);
-    console.log(myClaim);
-
-    return (
-        <>
-            {timeLeft ? <Timer timeLeft={timeLeft} /> : null}
-            <h2 className={styles.title}>Panel Content</h2>
-            <div className={styles.rowContainer}>
-                <p>Yooo</p>
-                <div className={styles.myParentContainer}></div>
-                <div className={styles.myClaims}></div>
-                {/* <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row />
-                <Row /> */}
-            </div>
-        </>
-    );
+  return (
+    <>
+      {timeLeft ? <Timer timeLeft={timeLeft} /> : null}
+      <h2 className={styles.title}>Panel Content</h2>
+      <div className={styles.rowContainer}>
+        <p>Yooo</p>
+        <div className={styles.myParentContainer}></div>
+        <div className={styles.myClaims}></div>
+        {/* <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row />
+        <Row /> */}
+      </div>
+    </>
+  );
 };
 
 export default HomePanel;
