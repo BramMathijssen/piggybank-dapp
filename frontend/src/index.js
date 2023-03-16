9bdc56413753c0c96ed32d29831c78cea3f37e95
Bram <Bram.mathijssen@gmail.com>
2023-03-16 21:00:45 +0100

    deleted transaction and token context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,21 +7,15 @@ import { EthersContextProvider } from "./context/ethers-context";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import { ChildContextProvider } from "./context/child-context";
-import { TransactionContextProvider } from "./context/transaction-context";
-import { TokenContextProvider } from "./context/token-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
             <EthersContextProvider>
-                    <ChildContextProvider>
-                        <TokenContextProvider>
-                            <TransactionContextProvider>
-                                <App />
-                            </TransactionContextProvider>
-                        </TokenContextProvider>
-                    </ChildContextProvider>
+                <ChildContextProvider>
+                    <App />
+                </ChildContextProvider>
             </EthersContextProvider>
         </BrowserRouter>
     </React.StrictMode>
